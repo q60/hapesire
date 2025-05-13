035f2cdb69cd68156fd77cc00ad49f34829665be
kira malinova <llathasa@outlook.com>
2025-05-13 22:08:39 +0300

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hapesire.MixProject do
   def project do
     [
       app: :hapesire,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :dev,
